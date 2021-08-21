7c9e4733ec89f4d0e1e636e6eb3c0626b438794c
James Walmsley <James.Walmsley@mottmac.com>
2021-08-22 11:26:39 +1200

    Add project files.

---
+++ ackermansFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ackermansFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
