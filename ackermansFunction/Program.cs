bbfb6a733a222c49bba811c27bd8db1f81e517a4
James Walmsley <James.Walmsley@mottmac.com>
2021-08-22 12:15:05 +1200

    Done, very easy to cause Stack Overflow Exception...

--- ackermansFunction/Program.cs
+++ ackermansFunction/Program.cs
@@ -6,7 +6,36 @@ namespace ackermansFunction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int m;
+            int n;
+            while (true)
+            {
+                Console.WriteLine("Give inputs 'm' and 'n' for my Ackerman's Function.");
+                Console.WriteLine("Enter any absolute integer value for 'm':");
+                m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter any absolute integer value for 'n':");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                int ackermansAnswer = AckermanFunc(m, n);
+                Console.WriteLine($"The result of the Ackerman's Function is: {ackermansAnswer}\n");
+            }
         }
+
+        public static int AckermanFunc(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+                return AckermanFunc(m - 1, 1);
+            }
+            else // m > 0 && n > 0
+            {
+                return AckermanFunc(m -1 , AckermanFunc(m, n - 1));
+            }
+        }
+
     }
 }
